9919dc339b7027e3c1f7537106d424340c4108be
rladenson <78043712+rladenson@users.noreply.github.com>
2021-12-31 07:45:03 -0700

    Make it a website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ServersetComponent } from "./serverset/serverset.component";
+import {IndexComponent} from "./index/index.component";
+import {ToDoComponent} from "./to-do/to-do.component";
 
 const routes: Routes = [
   { path: 'serverset', component: ServersetComponent },
-  { path: '', redirectTo: '/serverset', pathMatch: 'full' }
+  { path: '', component: IndexComponent },
+  { path: 'todo', component: ToDoComponent },
+  { path: 'server-set', redirectTo: '/serverset', pathMatch: 'full'},
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
