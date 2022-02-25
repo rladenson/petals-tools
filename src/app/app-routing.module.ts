604374a6525bb8db049eef74aa93c85d689bf84e
rladenson <78043712+rladenson@users.noreply.github.com>
2022-02-25 15:08:55 -0700

    Split SiteMap into its own component and gave the serversettings subpages their own router links/outlets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,30 @@ import { ServersetComponent } from "./serverset/serverset.component";
 import {IndexComponent} from "./index/index.component";
 import {ToDoComponent} from "./to-do/to-do.component";
 import {GroupSwitcherComponent} from "./group-switcher/group-switcher.component";
+import {SoloSettingsComponent} from "./solo-settings/solo-settings.component";
+import {BulkSettingsComponent} from "./bulk-settings/bulk-settings.component";
+import {TemplatesComponent} from "./templates/templates.component";
 
 const routes: Routes = [
-  { path: 'serverset', component: ServersetComponent },
   { path: '', component: IndexComponent },
+  { path: 'serverset',
+    component: ServersetComponent,
+    children: [
+      { path: 'solo', component: SoloSettingsComponent },
+      { path: 'bulk', component: BulkSettingsComponent },
+      { path: 'templates', component: TemplatesComponent },
+      { path: '**', redirectTo: 'solo' }
+    ]
+  },
   { path: 'todo', component: ToDoComponent },
   { path: 'groupswitcher', component: GroupSwitcherComponent},
+  //everything below here is aliases
   { path: 'server-set', redirectTo: '/serverset', pathMatch: 'full' },
   { path: 'serverset.html', redirectTo: '/serverset', pathMatch: 'full' },
   { path: 'todo.html', redirectTo: '/todo', pathMatch: 'full' },
   { path: 'to-do', redirectTo: '/todo', pathMatch: 'full' },
   { path: 'group-switcher', redirectTo: '/groupswitcher', pathMatch: 'full' },
+    //wildcard
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
