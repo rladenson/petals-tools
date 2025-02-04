b21d4ca068803293ffe4fa6bd43fd1155f55c346
rladenson <github@petalladenson.com>
2025-02-03 23:57:44 -0700

    feat: Modal

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	import { getContext } from 'svelte';
+
+	let { title, body, data = null, shown = $bindable(false) } = $props();
+
+	const modalShown: { value: boolean } = getContext('modalShown');
+
+	$effect(() => {
+		if (shown === true) showModal();
+	});
+
+	const hideModal = () => {
+		shown = false;
+		modalShown.value = false;
+	};
+	const showModal = () => {
+		shown = true;
+		modalShown.value = true;
+	};
+	let hideData = $state(true);
+</script>
+
+{#if shown}
+	<div class="absolute bottom-0 flex h-full w-full items-center justify-center bg-black/20">
+		<div class="flex h-fit w-fit max-w-96 flex-col rounded bg-white p-5">
+			<button class="self-end" aria-label="Close Modal" onclick={hideModal}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="size-6"
+					><path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" /></svg
+				>
+			</button>
+			<h1 class="text-2xl">{title}</h1>
+			<div class="text-l">{body}</div>
+			{#if data}
+				<div class="table border-collapse border">
+					<div class="table-header-group">
+						<div class="table-row">
+							<div class="table-cell border">
+								<button onclick={() => (hideData = !hideData)} aria-label="Toggle Data Shown">
+									{#if hideData}
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 24 24"
+											fill="currentColor"
+											class="inline size-6"
+										>
+											<path
+												fill-rule="evenodd"
+												d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
+												clip-rule="evenodd"
+											/>
+										</svg>
+									{:else}
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 -6 24 24"
+											fill="currentColor"
+											class="size-6"
+										>
+											<path
+												fill-rule="evenodd"
+												d="M12.53 16.28a.75.75 0 0 1-1.06 0l-7.5-7.5a.75.75 0 0 1 1.06-1.06L12 14.69l6.97-6.97a.75.75 0 1 1 1.06 1.06l-7.5 7.5Z"
+												clip-rule="evenodd"
+											/>
+										</svg>
+									{/if}
+								</button>
+								Data:
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="table-row-group {hideData ? 'h-0 overflow-clip' : ''}">
+					<div class="table-row">
+						<div class="table-cell border">
+							{JSON.stringify(data).replaceAll(/(?<=\"[,:])(?=\")/g, ' ')}
+						</div>
+					</div>
+				</div>
+			{/if}
+		</div>
+	</div>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { setContext } from 'svelte';
 	import '../app.css';
 	let { children } = $props();
 
@@ -7,10 +8,13 @@
 	function toggleSidebar() {
 		showMenu = !showMenu;
 	}
+
+	const modalShown = $state({ value: false });
+	setContext('modalShown', modalShown);
 </script>
 
-<div class="flex h-screen flex-col">
-	<header class="bg-blue-500 text-white sticky top-0 z-50">
+<div class="flex h-screen flex-col {modalShown.value ? 'overflow-hidden' : ''}">
+	<header class="sticky top-0 z-50 bg-blue-500 text-white">
 		<nav class="mx-auto flex items-center justify-between p-2" aria-label="Global">
 			<div class="flex lg:flex-1">
 				<button onclick={toggleSidebar} class="-m-1.5 p-1.5">
