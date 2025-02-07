5ff1a4e2b1172f4d5659f084bf59a5098900e6d2
rladenson <github@petalladenson.com>
2025-02-07 15:56:40 -0700

    feat: home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="p-5">
+	<h1 class="text-3xl">Petal's Tools</h1>
+	<a href="/serverset/solo" class="ml-3 text-blue-900 hover:text-blue-600">» Server Settings</a>
+	<p class="mt-5">
+		To review or suggest things message me on Discord (petalss_tm), to get help ask in the
+		<a
+			href="https://discord.com/channels/466707357099884544/912804952953479171"
+			class="text-blue-900 hover:text-blue-600"
+		>
+			#third-party-discussion
+		</a>
+		channel in the
+		<a href="https://discord.gg/PczBt78" class="text-blue-900 hover:text-blue-600">
+			PluralKit discord
+		</a>.
+	</p>
+</div>
